9278985305d42b0ad0ece518771ebfe9620e0997
Rock Dong <dongrock233@gmail.com>
2024-11-20 15:16:09 -0500

    Changed IP address

--- Front_End/src/components/LRButtons.js
+++ Front_End/src/components/LRButtons.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function LRButtons() {
     return (
         <div>
-            <Link to='http://localhost:3000/login'>
+            <Link to='login'>
                 <button className="buttons">Login</button>
             </Link>
 
-            <Link to='http://localhost:3000/register'>
+            <Link to='register'>
                 <button className="buttons">Register</button>
             </Link>
         </div>
